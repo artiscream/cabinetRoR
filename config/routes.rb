46a344f3412abfa9c88c4a4fea40e3da49193470
artiscream <arti_scream@ukr.net>
2019-01-18 16:45:20 +0200

    change root for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  get 'welcome/index'
+  get "welcome/index"
   # setting root directory to this welcome index page
-  root 'welcome#index'
+  root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+  	root "docs#index", as: "authenticated_root"
+  end
+
 
   resources :docs
 end
